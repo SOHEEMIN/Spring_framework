29fa38946ba3ef7d570cb1f04a3d1d635f83ba21
SOHEEMIN <minsohee94@naver.com>
2022-05-19 15:13:18 +0900

    login 완료, session 적용

---
+++ spring_member/src/main/java/com/its/member/HomeController.java
@@ -0,0 +1,26 @@
+package com.its.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/save-form")
+    public String save() {
+        return "save";
+    }
+    @GetMapping("/login-form")
+    public String login() {
+        return "login";
+    }
+    @GetMapping("/main")
+    public String main() {
+        return "main";
+    }
+
+}
---
+++ spring_member/src/main/java/com/its/member/Service/MemberService.java
@@ -0,0 +1,37 @@
+package com.its.member.Service;
+
+import com.its.member.DTO.MemberDTO;
+import com.its.member.Repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberService {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public boolean save (MemberDTO memberDTO){
+        int saveResult = memberRepository.save(memberDTO);
+        if(saveResult>0){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    public List<MemberDTO> findAll() {
+        List<MemberDTO> memberDTOList = memberRepository.findAll();
+        return memberDTOList;
+    }
+
+    public boolean login(MemberDTO memberDTO) {
+        boolean result = memberRepository.login(memberDTO);
+        if(result){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
